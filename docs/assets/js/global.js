d5df69219beff6f7956f5b21893f4730ebc44466
Taurit <git@taurit.pl>
2017-10-14 20:16:08 +0200

    Hide reference list by default, allow expanding (declutter article view)

--- docs/assets/js/global.js
+++ docs/assets/js/global.js
@@ -10,6 +10,30 @@ var toggleNav = function () {
 };
 var navbarButton = document.getElementById("navbar-button");
 navbarButton.addEventListener("click", toggleNav, false);
+// make reference list hidden by default to declutter article view
+var refListHeader = document.getElementById("reference-list");
+var refListContent = document.getElementById("reference-list-content");
+if (refListHeader !== null && refListContent !== null) {
+    // only some articles contain reference list
+    refListContent.style.display = 'none';
+    refListHeader.innerHTML = "<span id='show-reference-list'>Rozwiń listę odniesień ▾</button>";
+    function showReferenceList(ev) {
+        var refListHeader = document.getElementById("reference-list");
+        var refListContent = document.getElementById("reference-list-content");
+        refListHeader.innerHTML = "Lista odniesień";
+        refListContent.style.display = 'block';
+    }
+    var refListButton = document.getElementById("show-reference-list");
+    refListButton.onclick = showReferenceList;
+    // on in-text reference click, show reference list before navigating to it
+    var inTextReferences = document.getElementsByClassName
+        ? document.getElementsByClassName('resource-reference') // modern browsers
+        : document.querySelectorAll('.resource-reference'); // ie8
+    for (var i = 0; i < inTextReferences.length; ++i) {
+        var inTextReference = inTextReferences[i];
+        inTextReference.onclick = showReferenceList;
+    }
+}
 //google analytics
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
